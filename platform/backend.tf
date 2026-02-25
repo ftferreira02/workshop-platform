230ad33de561ac9f9793b772eba6cfe7dc12a3ef
ftferreira02 <tomasfreitas4054@gmail.com>
2026-02-25 17:21:40 +0000

    backend.tf fix

--- platform/backend.tf
+++ platform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "workshop-ua-terraform-state-bucket-ff2"
+    bucket         = "workshop-ua-dev-terraform-state-ff2"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
